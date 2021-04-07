e9d128248414e5254f22639d50c45d4d9541ab8e
LEONEL-POLLITO <81774877+LEONEL-POLLITO@users.noreply.github.com>
2021-04-06 21:29:36 -0500

    Update git.js

--- plugins/git.js
+++ plugins/git.js
@@ -1,10 +1,10 @@
 let handler = async m => m.reply(`
  *GIT DE ESTE BOT*
-https://github.com/Samu330/esubot
+MERRY Y LEONEL❤
 
 MY GIT:
 
-https://github.com/Samu330
+MERRY
 😴                                       
 `.trim()) 
 handler.help = ['git']
