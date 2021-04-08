e4e8c4458e80af5be6416c654dc2c3467f38dfd0
LEONEL-POLLITO <81774877+LEONEL-POLLITO@users.noreply.github.com>
2021-04-08 07:04:40 -0500

    Update setwelcome.js

--- plugins/setwelcome.js
+++ plugins/setwelcome.js
@@ -1,10 +1,14 @@
-const welcome = (number, groupname) => {
-    return `Hola @${number}. Bienvenido al grupo ${groupname} lee las reglas para no tener mal entendidos`
-}
-exports.welcome = welcome
 
-const bye = (number) => {
-    return `Adios @${number}. 👋`
+let handler = async (m, { conn, text, isROwner, isOwner }) => {
+  if (text) {
+    if (isROwner) global.conn.welcome = bienvenido
+    else if (isOwner) conn.welcome = text
+    else global.DATABASE._data.chats.sWelcome = text
+    m.reply('Welcome se a cambiado a:\n@user (Mention)\n@subject (nombre del grupo )')
+  } else throw 'Y el texto ?'
 }
-exports.bye = bye
-© 2021 GitHub, Inc.
+handler.help = ['setwelcome <teks>']
+handler.tags = ['owner', 'group']
+
+handler.command = /^setwelcome$/i
+module.exports = handler
